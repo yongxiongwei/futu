dcb895a30eaef10a192f5a7bb1d7ab3549145336
郭林 <450244484@qq.com>
2018-07-31 08:05:08 +0800

    Update client2.go

--- client2.go
+++ client2.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 
-	conn, err := net.Dial("tcp", "127.0.0.1:11111")
+	conn, err := net.Dial("tcp", "safe.lim.app:11113")
 	if err != nil {
 		fmt.Println("cannot connect server")
 		os.Exit(1)
